9ac98dae3fc57d41dc8c04a577a30a3eae0a0fbe
Gabriele Lopez <gabriele.ope@gmail.com>
2023-01-23 11:53:16 +0100

    .Cs file creation

---
+++ TicTacToeServer/TicTacToeMatch.cs
@@ -0,0 +1,25 @@
+﻿namespace TicTacToeServer;
+
+public class TicTacToeMatch
+{
+    public int[,] Board { get; set; }
+    public string User1 { get; set; }
+    public string User2 { get; set; }
+
+    public TicTacToeMatch()
+    {
+        this.Board = new int[3, 3];
+        User1 = "";
+        User2 = "";
+        TicTacToeMatchStatus = TicTacToeMatchStatus.Ongoing;
+    }
+    public TicTacToeMatchStatus TicTacToeMatchStatus { get; set; }
+}
+
+public enum TicTacToeMatchStatus
+{
+    Ongoing,
+    XWon,
+    OWon,
+    Draw
+}
