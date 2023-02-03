7cbb4e3682217657132894a9dc3239a5b2f964ed
Gabriele Lopez <gabriele.ope@gmail.com>
2023-02-03 10:52:42 +0100

    Request classes

---
+++ TicTacToeServer/Controllers/RequestBody/RequestBody.cs
@@ -0,0 +1,25 @@
+﻿namespace TicTacToeServer.Controllers.RequestBody;
+
+public class Location
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+}
+
+public class MakeMoveArguments
+{
+    public string Guid { get; set; }
+    public int Player { get; set; }
+    public Location Location { get; set; }
+}
+
+public class ConnectP2Arguments
+{
+    public string Guid { get; set; }
+    public string Username { get; set; }
+}
+
+public class CreateMatchArguments
+{
+    public string Username { get; set; }
+}
