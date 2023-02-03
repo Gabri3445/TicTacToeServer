f8d1d9d7a0702fc15e3c83c47170da114c95e6cb
Gabriele Lopez <gabriele.ope@gmail.com>
2023-02-03 10:53:37 +0100

    Response classes

---
+++ TicTacToeServer/Controllers/Responses/Responses.cs
@@ -0,0 +1,45 @@
+﻿namespace TicTacToeServer.Controllers.Responses;
+
+public class CreateMatchResponse
+{
+    public CreateMatchResponse(string guid)
+    {
+        Guid = guid;
+    }
+
+    public string Guid { get; set; }
+}
+
+public class GetBoardStatusResponse 
+{
+    public GetBoardStatusResponse(int rows, int columns, int[] flatBoard)
+    {
+        Rows = rows;
+        Columns = columns;
+        FlatBoard = flatBoard;
+    }
+
+    public int Rows { get; set; }
+    public int Columns { get; set; }
+    public int[] FlatBoard { get; set; }
+}
+
+public class GetPlayerResponse
+{
+    public GetPlayerResponse(int player)
+    {
+        Player = player;
+    }
+
+    public int Player { get; set; }
+}
+
+public class CheckWinResponse
+{
+    public CheckWinResponse(int matchStatus)
+    {
+        MatchStatus = matchStatus;
+    }
+
+    public int MatchStatus { get; set; }
+}
