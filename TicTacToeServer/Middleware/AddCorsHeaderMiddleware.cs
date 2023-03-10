b4f9c0ed6b229bc1602e1b10869a2f3b465b867d
Gabriele Lopez <gabriele.ope@gmail.com>
2023-03-10 20:42:25 +0100

    Fixed Cors middleware for Put, Post and other methods

--- TicTacToeServer/Middleware/AddCorsHeaderMiddleware.cs
+++ TicTacToeServer/Middleware/AddCorsHeaderMiddleware.cs
@@ -19,8 +19,17 @@ public class AddCorsHeaderMiddleware
         context.Response.OnStarting(() =>
         {
             context.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+            context.Response.Headers.Add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            context.Response.Headers.Add("Access-Control-Allow-Headers", "Content-Type");
             return Task.CompletedTask;
         });
+        
+        if (context.Request.Method == "OPTIONS")
+        {
+            context.Response.StatusCode = 200;
+            await context.Response.CompleteAsync();
+            return;
+        }
 
         await _next(context);
     }
